f1e582a3b8ad860fccf3ec910642fe38b4663153
segfault101 <vemurirahul93@gmail.com>
2015-12-08 07:59:53 -0700

    lemmatization complete  it took 5.5 hrs

---
+++ src/StanfordLemmatizer.java
@@ -0,0 +1,54 @@
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+public class StanfordLemmatizer {
+
+	PrintStream err = System.err;
+	
+    protected StanfordCoreNLP pipeline;
+
+    public StanfordLemmatizer() {
+        // Create StanfordCoreNLP object properties, with POS tagging
+        // (required for lemmatization), and lemmatization
+        Properties props;
+        props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        props.put("untokenizable", "noneDelete");
+      
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+
+    public String lemmatize(String documentText)
+    {
+//    	System.out.println("Before lemming: "+ documentText);
+    	
+        String lemmas = new String();
+        // Create an empty Annotation just with the given text
+        Annotation document = new Annotation(documentText);
+        // run all Annotators on this text
+        this.pipeline.annotate(document);
+        // Iterate over all of the sentences found
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+        for(CoreMap sentence: sentences) {
+            // Iterate over all tokens in a sentence
+            for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+                // Retrieve and add the lemma for each word into the
+                // list of lemmas
+                lemmas += " " + token.get(LemmaAnnotation.class);
+            }
+        }
+        
+//        System.out.println("After lemming: "+ lemmas);
+        
+        return lemmas;
+    }
+}
--- src/WIkiIndexer.java
+++ src/WIkiIndexer.java
@@ -34,6 +34,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 
 public class WIkiIndexer {
@@ -42,11 +46,39 @@ public class WIkiIndexer {
 		File FILE_DIR = new File("D:\\Desktop\\IR docs\\wiki-subset-20140602\\");
 //		System.out.println("Enter absolute file path:");
 //		String FILE_PATH = System.console().readLine();
-		
-		String querystr =  "the international electrotechnical commission";
+
+		//create lemmatizer
+		StanfordLemmatizer lemmatizer = new StanfordLemmatizer();
+
 //		System.out.println("Enter query :");
 //		String querystr = System.console().readLine();
+
+		String clue = "Daniel Hertzberg & James B. Stewart of this paper shared a 1988 Pulitzer for their stories about insider trading";
+		String category = "NEWSPAPERS";
+
+		//lemmatize the parts of query
+		clue = lemmatizer.lemmatize(clue);
+		category = lemmatizer.lemmatize(category);
 		
+		//concat to form query string
+		String querystr = clue + " " + category ;
+
+		
+		//construct a term to boost map to use with multifield query parser
+		Map<String, Float> boostMap = new HashMap<String, Float>();
+		
+		StringTokenizer catTokens = new StringTokenizer(category);
+		
+		while(catTokens.hasMoreTokens())
+			boostMap.put(catTokens.nextToken(), 1.2f);
+		
+		StringTokenizer clueTokens = new StringTokenizer(clue);
+
+		while(clueTokens.hasMoreTokens())
+			boostMap.put(clueTokens.nextToken(), 1.0f);
+		
+		
+		//flag
 		int useExistingIndex = 0;
 		
 		// Specify the analyzer for tokenizing text when indexing and searching
@@ -65,18 +97,28 @@ public class WIkiIndexer {
 		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
 		IndexWriter w = new IndexWriter(index, config);
 
+		
+		
+		
+		
+//		String lem = lemmatizer.lemmatize("[[British Standards]] CATEGORIES: 1901 establishments in the United Kingdom, British Standards, International Electrotechnical Commission, Standards organizations, Certification marks, Organisations based in the United Kingdom British Standards are the standards produced by BSI Group which is incorporated under a Royal Charter (and which is formally designated as the National Standards Body (NSB) for the UK). The BSI Group produces British Standards under the authority of the Charter, which lays down as one of the BSI's objectives to: Formally, as per the 2002 Memorandum of Understanding between the BSI and the United Kingdom Government, British Standards are defined as: Products and services which BSI certifies as having met the requirements of specific standards within designated schemes are awarded the Kitemark.[tpl]cite web|url=http://www.kitemark.com/about-kitemark/ |title=Kitemark.com |publisher=Kitemark.com |date= |accessdate=2012-04-03[/tpl] ==How British Standards are made== The BSI Group as a whole does not produce British Standards, as standards work within the BSI is decentralized.  The governing Board of BSI establishes a Standards Board.  The Standards Board does little apart from setting up Sector Boards (a Sector in BSI parlance being a field of standardization such as ICT, Quality, Agriculture, Manufacturing, or Fire).  Each Sector Board in turn constitutes several Technical Committees.  It is the Technical Committees that, formally, approve a British Standard, which is then presented to the Secretary of the supervisory Sector Board for endorsement of the fact that the Technical Committee has indeed completed a task for which it was constituted. ==The standards== The standards produced are titled British Standard XXXX-P:YYYY where XXXX is the number of the standard, P is the number of the part of the standard (where the standard is split into multiple parts) and YYYY is the year in which the standard came into effect.  BSI Group currently has over 27,000 active standards. Products are commonly specified as meeting a particular British Standard, and in general this can be done without any certification or independent testing. The standard simply provides a shorthand way of claiming that certain specifications are met, while encouraging manufacturers to adhere to a common method for such a specification. The Kitemark can be used to indicate certification by BSI, but only where a Kitemark scheme has been set up around a particular standard. It is mainly applicable to safety and quality management standards. There is a common misunderstanding that Kitemarks are necessary to prove compliance with any BS standard, but in general it is neither desirable nor possible that every standard be 'policed' in this way. [ref][tpl]cite conference|booktitle=International Symposium on Plastics Testing and Standardization|volume=247|series=ASTM special technical publication|publisher=American Society for Testing Materials International|year=1959|title=Standardization in the United Kingdom|author=H.M. Glass G. Weston|pages=37–38[/tpl][tpl]cite book|chapter=Standards, Specifications, and codes of practice|author=J.M. Faller and M.H. Graham|title=Handbook of Electrical Installation Practice|editor=Geoffrey Stokes|edition=4th|publisher=Wiley-Blackwell|year=2003|isbn=0-632-06002-6|isbn=978-0-632-06002-3|pages=305–306[/tpl][tpl]cite book|title=The constitution of private governance: product standards in the regulation of integrating markets|volume=4|series=International studies in the theory of private law|author=Harm Schepel|publisher=Hart Publishing|year=2005|isbn=1-84113-487-2|isbn=978-1-84113-487-1|pages=121–124[/tpl][tpl]Cite journal|title=MEMORANDUM OF UNDERSTANDING BETWEEN THE UNITED KINGDOM GOVERNMENT AND THE BRITISH STANDARDS INSTITUTION IN RESPECT OF ITS ACTIVITIES AS THE UNITED KINGDOM'S NATIONAL STANDARDS BODY|publisher=United Kingdom Department for Business, Innovation, and Skills|year=2002|format=PDF|url=http://bis.gov.uk/files/file11950.pdf[/tpl]==External links==");
+		
 		// if an index doesnt already exists 
 		// index the docs of files in specified directory
 		if(useExistingIndex == 0)
 			for(File f : FILE_DIR.listFiles())
-				indexDocsInFile(f, w);
-		
+			{
+				System.out.println("Processing File : "+ f.toString());
+				indexDocsInFile(f, w, lemmatizer);
+			}
+
 		//close index writer
 		w.close();
 
 		// the "content" arg specifies the default field to use
 		// when no field is explicitly specified in the query.
-		MultiFieldQueryParser queryParser = new MultiFieldQueryParser(Version.LUCENE_40, new String[] {"categories", "content"}, analyzer);
+		
+		MultiFieldQueryParser queryParser = new MultiFieldQueryParser(Version.LUCENE_40, new String[] {"categories", "content"}, analyzer, boostMap); //, boostMap );
 		Query q = queryParser.parse(querystr);
 
 
@@ -138,7 +180,7 @@ public class WIkiIndexer {
 
 
 
-	private static void indexDocsInFile(File file, IndexWriter w) throws FileNotFoundException, IOException {
+	private static void indexDocsInFile(File file, IndexWriter w, StanfordLemmatizer lemmatizer) throws FileNotFoundException, IOException {
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -161,11 +203,11 @@ public class WIkiIndexer {
 								
 				//store the prev doc
 				Document doc = new Document();
+								
+				categories = new TextField("categories", lemmatizer.lemmatize(tempCategories), Field.Store.YES);
+				categories.setBoost(1.2f);	//categories act as tier 1
 				
-				categories = new TextField("categories", tempCategories, Field.Store.YES);
-				categories.setBoost(2.0f);	//categories act as tier 1
-				
-				content = new TextField("content", tempContent, Field.Store.YES);				
+				content = new TextField("content", lemmatizer.lemmatize(tempContent), Field.Store.YES);				
 				
 				doc.add(title);
 				doc.add(categories);
@@ -188,10 +230,10 @@ public class WIkiIndexer {
 			{
 				
 				if(line.startsWith("CATEGORIES:"))
-					tempCategories += line;
+					tempCategories += " " +line;
 				
 				else
-					tempContent += line;
+					tempContent += " " + line;
 			}
 						
 		}
