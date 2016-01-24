1be71e508e90bd5310998e15f9869badf9a8c6c5
Rahul <vemurirahul93@gmail.com>
2016-01-24 13:13:31 -0700

    Update WIkiIndexer.java

--- src/WIkiIndexer.java
+++ src/WIkiIndexer.java
@@ -97,12 +97,7 @@ public class WIkiIndexer {
 		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
 		IndexWriter w = new IndexWriter(index, config);
 
-		
-		
-		
-		
-//		String lem = lemmatizer.lemmatize("[[British Standards]] CATEGORIES: 1901 establishments in the United Kingdom, British Standards, International Electrotechnical Commission, Standards organizations, Certification marks, Organisations based in the United Kingdom British Standards are the standards produced by BSI Group which is incorporated under a Royal Charter (and which is formally designated as the National Standards Body (NSB) for the UK). The BSI Group produces British Standards under the authority of the Charter, which lays down as one of the BSI's objectives to: Formally, as per the 2002 Memorandum of Understanding between the BSI and the United Kingdom Government, British Standards are defined as: Products and services which BSI certifies as having met the requirements of specific standards within designated schemes are awarded the Kitemark.[tpl]cite web|url=http://www.kitemark.com/about-kitemark/ |title=Kitemark.com |publisher=Kitemark.com |date= |accessdate=2012-04-03[/tpl] ==How British Standards are made== The BSI Group as a whole does not produce British Standards, as standards work within the BSI is decentralized.  The governing Board of BSI establishes a Standards Board.  The Standards Board does little apart from setting up Sector Boards (a Sector in BSI parlance being a field of standardization such as ICT, Quality, Agriculture, Manufacturing, or Fire).  Each Sector Board in turn constitutes several Technical Committees.  It is the Technical Committees that, formally, approve a British Standard, which is then presented to the Secretary of the supervisory Sector Board for endorsement of the fact that the Technical Committee has indeed completed a task for which it was constituted. ==The standards== The standards produced are titled British Standard XXXX-P:YYYY where XXXX is the number of the standard, P is the number of the part of the standard (where the standard is split into multiple parts) and YYYY is the year in which the standard came into effect.  BSI Group currently has over 27,000 active standards. Products are commonly specified as meeting a particular British Standard, and in general this can be done without any certification or independent testing. The standard simply provides a shorthand way of claiming that certain specifications are met, while encouraging manufacturers to adhere to a common method for such a specification. The Kitemark can be used to indicate certification by BSI, but only where a Kitemark scheme has been set up around a particular standard. It is mainly applicable to safety and quality management standards. There is a common misunderstanding that Kitemarks are necessary to prove compliance with any BS standard, but in general it is neither desirable nor possible that every standard be 'policed' in this way. [ref][tpl]cite conference|booktitle=International Symposium on Plastics Testing and Standardization|volume=247|series=ASTM special technical publication|publisher=American Society for Testing Materials International|year=1959|title=Standardization in the United Kingdom|author=H.M. Glass G. Weston|pages=37–38[/tpl][tpl]cite book|chapter=Standards, Specifications, and codes of practice|author=J.M. Faller and M.H. Graham|title=Handbook of Electrical Installation Practice|editor=Geoffrey Stokes|edition=4th|publisher=Wiley-Blackwell|year=2003|isbn=0-632-06002-6|isbn=978-0-632-06002-3|pages=305–306[/tpl][tpl]cite book|title=The constitution of private governance: product standards in the regulation of integrating markets|volume=4|series=International studies in the theory of private law|author=Harm Schepel|publisher=Hart Publishing|year=2005|isbn=1-84113-487-2|isbn=978-1-84113-487-1|pages=121–124[/tpl][tpl]Cite journal|title=MEMORANDUM OF UNDERSTANDING BETWEEN THE UNITED KINGDOM GOVERNMENT AND THE BRITISH STANDARDS INSTITUTION IN RESPECT OF ITS ACTIVITIES AS THE UNITED KINGDOM'S NATIONAL STANDARDS BODY|publisher=United Kingdom Department for Business, Innovation, and Skills|year=2002|format=PDF|url=http://bis.gov.uk/files/file11950.pdf[/tpl]==External links==");
-		
+
 		// if an index doesnt already exists 
 		// index the docs of files in specified directory
 		if(useExistingIndex == 0)
@@ -238,8 +233,7 @@ public class WIkiIndexer {
 						
 		}
 		
-//		System.out.println(tempContent);	
-		
+
 		br.close();
 	}
 	
